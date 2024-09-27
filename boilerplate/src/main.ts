318776857bbed6f58d68b496dae48aa4010d2642
Jong1co <narira0922@gmail.com>
2024-09-28 00:35:51 +0900

    feat: boilerplate 수정

--- boilerplate/src/main.ts
+++ boilerplate/src/main.ts
@@ -4,6 +4,38 @@ window.addEventListener("load", () => {
   init();
 });
 
-function init() {
-  console.log(THREE);
+async function init() {
+  const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+  });
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.outputColorSpace = THREE.SRGBColorSpace;
+
+  document.body.appendChild(renderer.domElement);
+
+  const scene = new THREE.Scene();
+
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    500
+  );
+
+  render();
+
+  function render() {
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  }
+
+  function handleResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  window.addEventListener("resize", handleResize);
 }
